9f05fca24ff27674761c0029f45264fb054e7fa5
Paul <piratium3@gmail.com>
2025-05-13 16:32:06 +0300

    Отработка массивов через циклы, Linq и Array

---
+++ Array/Array/Program.cs
@@ -0,0 +1,84 @@
+﻿
+
+int[] ints = [2, 4];
+int[] ints1 = new int[5] { 1, 2, 3, 4, 5 };
+
+Random random = new Random();
+
+
+int[] myArray = Enumerable.Range(1, 100).Select(_ => random.Next(1,20)).ToArray();
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write($"{myArray[i]} ");
+}
+
+Console.WriteLine();
+
+int[] myArray2 = new int[10];
+
+Console.WriteLine("Введите числа для массива");
+
+for (int i = 0; i < myArray2.Length; i++)
+{
+    myArray2[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine();
+
+foreach (int i in myArray2)
+{
+    Console.Write(i + " ");
+}
+
+
+int length = 5;
+
+for (int i = 0; i < length + 1; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write("#");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+for (int i = length; i > 0; i--)
+{
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write("#");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+for (int i = length; i > 0; i--)
+{
+    for (int j = 0; j < i - 1; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int k = 0; k <= length - i; k++)
+    {
+        Console.Write("#");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = length; i > 0; i--)
+{
+    for (int j = 0; j < length - i; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int k = 0; k <= i - 1; k++)
+    {
+        Console.Write("#");
+    }
+    Console.WriteLine();
+}
---
+++ Array/Array2/Program.cs
@@ -0,0 +1,81 @@
+﻿Random random = new Random();
+
+int[] ints = Enumerable.Range(1,10).Select(_ => random.Next(20, 40)).ToArray();
+
+for (int i = ints.Length - 1; i >= 0; i--)
+{
+    Console.Write(ints[i] + " ");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+foreach (int i in ints)
+{
+    Console.Write(i + " ");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+int sum = 0;
+
+for (int i = 0; i < ints.Length; i++)
+{
+    if (ints[i] % 2 == 0)
+    {
+        sum += ints[i];
+    }
+}
+
+Console.WriteLine($"Sum = {sum}");
+
+int sumLinq = ints.Where(el => el % 2 == 0).Sum();
+
+Console.WriteLine($"SumLinq = {sumLinq}");
+Console.WriteLine();
+
+int min = ints[3];
+
+for (int i = 0; i < ints.Length; i++)
+{
+    if (ints[i] < min)
+        min = ints[i];
+}
+
+Console.WriteLine($"Min = {min}");
+
+int minLinq = ints.Min();
+
+Console.WriteLine($"MinLinq = {minLinq}");
+
+Console.WriteLine();
+Console.WriteLine("Массив из уникальных элементов:");
+for (int i = 0; i < ints.Length; i++)
+{
+    bool isUnique = true;
+
+    for (int j = 0; j < i; j++)
+    {
+        if (ints[i] == ints[j])
+        {
+            isUnique = false;
+        }
+    }
+
+    if (isUnique)
+    {
+        Console.Write(ints[i] + " ");
+    }
+}
+
+Console.WriteLine("\n\nМассив из уникальных элементов с linq");
+Console.WriteLine(String.Join(" ", ints.Distinct().ToArray()) + "\n");
+
+Console.WriteLine("Отсортированный массив c linq:");
+Console.WriteLine(String.Join(" ", ints.OrderBy(el => el).ToArray()));
+
+Console.WriteLine("\nОтсортированный массив c методом Sort() класса Array:");
+Array.Sort(ints);
+Console.WriteLine(String.Join(" ", ints));
+
