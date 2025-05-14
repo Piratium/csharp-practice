e7e88dc4f297cbc93bf9d6d0cc06a0eb876da7c6
Paul <piratium3@gmail.com>
2025-05-14 17:46:11 +0300

    Range, Index, Многомерные/зубчатые массивы

---
+++ MultiDimensional array/MultiDimensional array/Program.cs
@@ -0,0 +1,117 @@
+﻿int[,] myTwoDimensionalArray = new int[,] { { 2, 3, 4, 2 }, { 2, 4, 2, 1 }, { 1, 2, 5, 2 } };
+
+int rows = myTwoDimensionalArray.GetLength(0);
+int columns = myTwoDimensionalArray.GetLength(1);
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write(myTwoDimensionalArray[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+// Заполненение рандомными числами
+int[,] nextArray = new int[2,5];
+
+rows = nextArray.GetLength(0);
+columns = nextArray.GetLength(1);
+Random random = new Random();
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        nextArray[i,j] = random.Next(10);
+    }
+}
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write(nextArray[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+// Ручное заполнение числами через консоль
+//string[,] handleCreateArray = new string[2,3];
+//rows = handleCreateArray.GetLength(0);
+//columns = handleCreateArray.GetLength(1);
+
+//for (int i = 0; i < rows; i++)
+//{
+//    for (int j = 0; j < columns; j++)
+//    {
+//        Console.WriteLine($"Row: {i} Column: {j}");
+//        handleCreateArray[i, j] = Console.ReadLine();
+//    }
+//}
+
+//for (int i = 0; i < rows; i++)
+//{
+//    for (int j = 0; j < columns; j++)
+//    {
+//        Console.Write(handleCreateArray[i, j] + "\t");
+//    }
+//    Console.WriteLine();
+//}
+
+// Зубчатый массив
+int[][] myJaggedArray = new int[3][];
+
+myJaggedArray[0] = new int[3];
+myJaggedArray[1] = new int[2];
+myJaggedArray[2]  = new int[4];
+
+for (int i = 0; i < myJaggedArray.Length; i++)
+{
+    for (int j = 0; j < myJaggedArray[i].Length; j++)
+    {
+        myJaggedArray[i][j] = random.Next(10);
+    }
+}
+
+for (int i = 0; i < myJaggedArray.Length; i++)
+{
+    for (int j = 0; j < myJaggedArray[i].Length; j++)
+    {
+        Console.Write(myJaggedArray[i][j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+// Трехмерный зубчатый массив
+int[][][] jagaJagaArray = new int[random.Next(3, 5)][][];
+
+for (int i = 0; i < jagaJagaArray.Length; i++)
+{
+    jagaJagaArray[i] = new int[random.Next(3, 6)][];
+    for (int j = 0; j < jagaJagaArray[i].Length; j++)
+    {
+        jagaJagaArray[i][j] = new int[random.Next(3, 6)];
+        for (int k = 0; k < jagaJagaArray[i][j].Length; k++)
+        {
+            jagaJagaArray[i][j][k] = random.Next(10);
+        }
+    }
+}
+
+for (int i = 0; i < jagaJagaArray.Length; i++)
+{
+    Console.WriteLine($"Массив массивов №{i+1}");
+    for (int j = 0; j < jagaJagaArray[i].Length; j++)
+    {
+        for (int k = 0; k < jagaJagaArray[i][j].Length; k++)
+        {
+            Console.Write(jagaJagaArray[i][j][k] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
---
+++ RangesAndIndices/RangesAndIndices/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] ints = Enumerable.Range(1,15).ToArray();
+
+Console.WriteLine(String.Join(" ", ints) +"\n");
+
+Console.WriteLine(String.Join(" ", ints[4..^2]));
+
+// Index myIndex = ^1;
+Index myIndex = new Index(1, true);
+Console.WriteLine(ints[myIndex]);
+
+// Range myRange = 1..5
+Range myRange = new Range(1,5);
+Console.WriteLine(string.Join(" ", ints[myRange]));
+
+string myString = "Hello world! :D";
+Console.WriteLine(myString[6..11]);
+Console.WriteLine(myString[^2..^0]);
+
+foreach (var i in myString[3..5])
+    Console.Write(i);
+Console.WriteLine(myString[^6]);
