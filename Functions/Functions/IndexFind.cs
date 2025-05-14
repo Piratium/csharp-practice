3357acd66d1b3ea5829799fb864019a88a77042c
Paul <piratium3@gmail.com>
2025-05-14 19:13:58 +0300

    Перегруз методов

---
+++ Functions/Functions/IndexFind.cs
@@ -0,0 +1,33 @@
+﻿static class IndexFind
+{
+    /// <summary>
+    /// Возвращает индекс определенного элемента
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    public static int MyIndex (this int[] arr, int num)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == num)
+                return i;
+        }
+        return -1;
+    }
+
+    public static int MyIndex (this string myString, char symbol)
+    {
+        for (int i = 0; i < myString.Length; i++)
+        {
+            if (myString[i] == symbol)
+                return i;
+        }
+        return -1;
+    }
+
+    public static void PrintMyIndex(this int myIndex)
+    {
+        Console.WriteLine($"Найденный индекс коллекции: {myIndex}");
+    }
+}
---
+++ Functions/Functions/Program.cs
@@ -0,0 +1,22 @@
+﻿void WriteUserName ()
+{
+    Console.Write("Введите своё имя: ");
+    Console.WriteLine($"\nПривет, {Console.ReadLine()}!");
+}
+
+//WriteUserName();
+
+int[] myArray = { 6, 3, 1, 6, 4, 8, 2, 1, 3, 9 };
+foreach (int i in myArray)
+    Console.Write(i + " ");
+
+Console.WriteLine("\n");
+
+myArray.MyIndex(2).PrintMyIndex();
+
+string strr = "Hello";
+strr.MyIndex('l').PrintMyIndex();
+
+
+
+
