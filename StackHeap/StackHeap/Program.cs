721d70478e9cd4c4b7cffeeaeb23132f72f31cff
Paul <piratium3@gmail.com>
2025-05-15 15:54:04 +0300

    Наглядный пример, как очищаются данные из стека и кучи

---
+++ StackHeap/StackHeap/Program.cs
@@ -0,0 +1,28 @@
+﻿static void Foo (int myInt)
+{
+    myInt = 11;
+}
+
+static void Bar (int[] myArr)
+{
+    myArr[0] = 1;
+}
+
+int a = 1;
+
+Foo(a);
+Console.WriteLine(a);
+
+int[] arr = new int[3];
+Console.WriteLine("Массив исходный");
+foreach (int item in arr)
+    Console.Write(item + " ");
+
+Bar(arr);
+Console.WriteLine();
+Console.WriteLine("\nМассив измененный");
+foreach (int item in arr)
+    Console.Write(item + " ");
+
+// Шок, но string - ссылочный тип
+string myStr = "sad";
